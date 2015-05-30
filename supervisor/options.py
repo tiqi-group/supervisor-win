3019017b5728a355962c56e997179b4b8fd06729
Alex Sandro <geniofuturo@gmail.com>
2015-05-30 02:42:45 -0300

    Remove old code.

--- supervisor/options.py
+++ supervisor/options.py
@@ -1278,60 +1278,6 @@ class ServerOptions(Options):
     def dropPrivileges(self, user):
         return
 
-    def _dropPrivileges(self, user):
-        # Drop root privileges if we have them
-        if user is None:
-            return "No user specified to setuid to!"
-
-        # get uid for user, which can be a number or username
-        try:
-            uid = int(user)
-        except ValueError:
-            try:
-                pwrec = pwd.getpwnam(user)
-            except KeyError:
-                return "Can't find username %r" % user
-            uid = pwrec[2]
-        else:
-            try:
-                pwrec = pwd.getpwuid(uid)
-            except KeyError:
-                return "Can't find uid %r" % uid
-
-        current_uid = os.getuid()
-
-        if current_uid == uid:
-            # do nothing and return successfully if the uid is already the
-            # current one.  this allows a supervisord running as an
-            # unprivileged user "foo" to start a process where the config
-            # has "user=foo" (same user) in it.
-            return
-
-        if current_uid != 0:
-            return "Can't drop privilege as nonroot user"
-
-        gid = pwrec[3]
-        if hasattr(os, 'setgroups'):
-            user = pwrec[0]
-            groups = [grprec[2] for grprec in grp.getgrall() if user in
-                      grprec[3]]
-
-            # always put our primary gid first in this list, otherwise we can
-            # lose group info since sometimes the first group in the setgroups
-            # list gets overwritten on the subsequent setgid call (at least on
-            # freebsd 9 with python 2.7 - this will be safe though for all unix
-            # /python version combos)
-            groups.insert(0, gid)
-            try:
-                os.setgroups(groups)
-            except OSError:
-                return 'Could not set groups of effective user'
-        try:
-            os.setgid(gid)
-        except OSError:
-            return 'Could not set group id of effective user'
-        os.setuid(uid)
-
     def waitpid(self):
         stopped = []
         for pid in self.child_process:
