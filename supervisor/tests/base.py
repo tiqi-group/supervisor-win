674c3be4b481393f4c7fcc6c8060191d6c1b00e5
Mike Naberezny <mike@naberezny.com>
2018-01-01 02:38:44 -0200

    Remove unused import
    (cherry picked from commit 9357d8d573742086167523e71c2bb8774a5bc7a6)

--- supervisor/tests/base.py
+++ supervisor/tests/base.py
@@ -1,18 +1,14 @@
+_NOW = 1151365354
+_TIMEFORMAT = '%b %d %I:%M %p'
+
 import os
 import sys
 import tempfile
 
 from supervisor.helpers import DummyPopen
-
-_NOW = 1151365354
-_TIMEFORMAT = '%b %d %I:%M %p'
-
-from supervisor.compat import (
-    Fault,
-    as_bytes,
-    as_string,
-    total_ordering
-)
+from supervisor.compat import total_ordering
+from supervisor.compat import Fault
+from supervisor.compat import as_bytes
 
 # mock is imported here for py2/3 compat.  we only declare mock as a dependency
 # via tests_require so it is not available on all supervisor installs.  the
