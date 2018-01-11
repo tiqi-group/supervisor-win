e46cff431d97bcf624044262aa583496545bdfc7
cclauss <cclauss@bluewin.ch>
2018-01-11 18:21:21 -0200

    Tidy up those trove classifiers
    To be in sync with lines 20-23.
    
    (cherry picked from commit c5c3367b46dd78b57e40091081ab04cd6916f215)

--- setup.py
+++ setup.py
@@ -53,10 +53,10 @@ CLASSIFIERS = [
     'Topic :: System :: Monitoring',
     'Topic :: System :: Systems Administration',
     "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
 ]
 
