f7445e110f664037e146e40ee84673f06573bdbe
alexsilva <geniofuturo@gmail.com>
2018-11-28 14:14:33 -0200

    Test fix: test_removelogs

--- supervisor/tests/test_dispatchers.py
+++ supervisor/tests/test_dispatchers.py
@@ -1035,7 +1035,7 @@ class PEventListenerDispatcherTests(unittest.TestCase):
     def test_removelogs(self):
         options = DummyOptions()
         config = DummyPConfig(options, 'process1', '/bin/process1',
-                              stdout_logfile='/tmp/foo')
+                              stdout_logfile=os.path.join(tempfile.gettempdir(), 'foo.txt'))
         process = DummyProcess(config)
         dispatcher = self._makeOne(process)
         dispatcher.removelogs()
