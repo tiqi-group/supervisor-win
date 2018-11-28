30b48fd154994fafdfd7dde7d7cacd2d15431a4e
alexsilva <geniofuturo@gmail.com>
2018-11-28 14:07:42 -0200

    Test fix: test_reopenlogs

--- supervisor/tests/test_dispatchers.py
+++ supervisor/tests/test_dispatchers.py
@@ -159,7 +159,7 @@ class POutputDispatcherTests(unittest.TestCase):
     def test_reopenlogs(self):
         options = DummyOptions()
         config = DummyPConfig(options, 'process1', '/bin/process1',
-                              stdout_logfile='/tmp/foo')
+                              stdout_logfile=os.path.join(tempfile.gettempdir(), 'foo.txt'))
         process = DummyProcess(config)
         dispatcher = self._makeOne(process)
         dispatcher.reopenlogs()
