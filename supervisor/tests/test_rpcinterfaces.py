961d8682004a8d5931f6af000ab6f40f61fa49d1
alexsilva <geniofuturo@gmail.com>
2019-06-21 20:03:42 -0300

    Test fix: test_tailProcessStdoutLog_none

--- supervisor/tests/test_rpcinterfaces.py
+++ supervisor/tests/test_rpcinterfaces.py
@@ -1534,7 +1534,7 @@ class SupervisorNamespaceXMLRPCInterfaceTests(TestBase):
         from supervisor.compat import letters
         options = DummyOptions()
         pconfig = DummyPConfig(options, 'foo', '/bin/foo',
-                               stdout_logfile='/tmp/fooooooo')
+                               stdout_logfile=TempFileOpen.make_path('stdout-logfile-6.log'))
         supervisord = PopulatedDummySupervisor(options, 'foo', pconfig)
         interface = self._makeOne(supervisord)
         process = supervisord.process_groups['foo'].processes['foo']
